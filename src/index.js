674e6fca29f5a99526feefcbf2615cc65c673e41
mashaloiko <loiko_masha@mail.ru>
2022-09-13 22:29:41 +0300

    fix: renamed module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { render } from './js/initialRender.js';
 import { eventListeners } from './js/eventListeners.js';
 import { createTime } from './js/createTime.js';
-import { getUser } from './js/requests.js';
+import { getUser } from './js/requestGithub.js';
 render();
 eventListeners();
 createTime();
